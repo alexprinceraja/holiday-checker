c030e0c97fb5a0e145bc78d4afbe0830e0983389
Alexprinceraja <alexprinceraja@gmail.com>
2025-09-01 10:07:34 +0200

    Initial commit

---
+++ src/main/java/com/example/holiday_checker/HolidayCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.holiday_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holiday_checker/HolidayCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holiday_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
